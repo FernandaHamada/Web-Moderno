e0aa877035a616533d253dabbaae5ce6ab889e67
Fernanda Hamada <fernandakubayashi@gmail.com>
2020-01-19 11:01:38 -0300

    Número: Alguns Cuidados

---
+++ numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) //Infinity
+console.log("10" / 2) //convetido para valor numérico '10'
+console.log("Show" * 2) // Not a Number
+console.log(0.1 + 0.7) // ponto flutuante, 0.799999...
+//console.log(10.toString())
+console.log((10.345).toFixed(2)) // 10.35
