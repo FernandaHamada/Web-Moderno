d5b22530fd5dec1cd0fd6594d84725f1a3e06fde
Fernanda Hamada <fernandakubayashi@gmail.com>
2020-01-22 14:42:50 -0300

    Declaração de Variáveis com Var 2

---
+++ var2.js
@@ -0,0 +1,8 @@
+var numero = 1
+{
+    var numero = 2
+    console.log('dentro = ', numero) // retorna 'dentro = 2'
+}
+console.log('fora = ', numero) // retorna 'fora = 2'
+
+// cuidado com o escopo global
