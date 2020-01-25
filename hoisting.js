94abfb8eccdc391a7b7069d17ec804ef968a2c7b
Fernanda Hamada <fernandakubayashi@gmail.com>
2020-01-25 14:12:31 -0300

    Hoisting

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+console.log("a = ", a) // retorna 'undefined'
+var a = 2
+console.log('a =', a) // retorna 'a = 2'
+// içamento da variável declarada com VAR
+
+console.log("b =", b)
+let b = 2
+console.log("b =", b) // Cannot access 'b' before initialization
+// processo de içamento não ocorre com LET
