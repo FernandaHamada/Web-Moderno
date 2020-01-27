2644bf3baff7ff58921982d49d8f1c89fcf53698
Fernanda Hamada <fernandakubayashi@gmail.com>
2020-01-27 11:06:47 -0300

    Operadores: Destructuring

---
+++ opDestructuring.js
@@ -0,0 +1,28 @@
+// novo recurso ES2015
+
+const pessoa = {
+    nome: "Ana",
+    idade: 5,
+    endereco: {
+        logradouro: "Rua ABC",
+        numero: 1000
+    }
+}
+
+const {nome, idade} = pessoa
+console.log(nome, idade) // "Ana 5"
+
+const {nome: n, idade: i} = pessoa
+console.log(n, i) // "Ana 5"
+
+const {sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada) // 'undefined true'
+
+
+
+const {endereco: {logradouro, numero, cep} } = pessoa
+console.log(logradouro, numero, cep) // 'Rua ABC 1000 undefined'
+
+const { conta: { ag, num} } =  pessoa
+console.log(ag, num) // retorna "Cannot destructure property `ag` of 'undefined' or 'null'."
+
