7cfabaf0485890a7882dd6266d874f2085246696
Fernanda Hamada <fernandakubayashi@gmail.com>
2020-01-30 10:27:35 -0300

    Estrutura FOR

---
+++ Controle/for1.js
@@ -0,0 +1,15 @@
+let contandor = 1
+while(contandor <= 10){
+    console.log(`contador = ${contandor}`)
+    contandor++
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log(`i = ${i}`)
+}
+
+// length = comprimento
+const notas = [6.7, 7.4, 8.9, 8.1, 7.7]
+for(let i = 0; i < notas.length; i++){
+    console.log(`Notas = ${notas[i]}`)
+}
