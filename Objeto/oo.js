142cee5ac7d94c16ba940aa0823b4229d598d541
Fernanda Hamada <fernandakubayashi@gmail.com>
2020-03-31 13:36:32 -0300

    Introdução OO

---
+++ Objeto/oo.js
@@ -0,0 +1,24 @@
+// CÓDIGO NÃO EXECUTÁVEL
+// Procedural
+
+processamento(valor1, valor2, valor3) // funções que manipulam dados
+
+// OO - dados personalidados(diz quais são os atributos)
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() // foco passou a ser o objeto
+
+/* "mapaer o mundo em cima de objetos", mudança de foco*/
+
+// Princípios importantes:
+// 1 - abstração: traduzir um obj para o sistema, por necessidade de cada caso
+// 2 - encapsulamento: detalhes de implementação ocultos, dentro do objeto. Interface de comunicação
+// 3 - herança (prototype): recebe atributos ,comportamentos e caracteristicas de um objeto 'pai'
+// 4 - polimorfismo: conceito generico, consegue substituir por conceitos concretos (multiplas formas) 
